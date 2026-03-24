1f96d7859fb9c32f3ad52547fdd58610d787e9d2
Rainha5 <rainha5@LAPTOP-IAV9S8SC.localdomain>
2026-03-24 21:45:37 +0000

    teste

--- tests/test_dividir.py
+++ tests/test_dividir.py
@@ -7,3 +7,12 @@ def test_dividir_normal():
 def test_dividir_por_zero():
     with pytest.raises(ValueError):
         dividir(10, 0)
+
+def test_dividir_resultado_errado():
+    assert dividir(10, 2) == 6  
+
+def test_dividir_negativos():
+    assert dividir(-10, 2) == 10  
+
+def test_dividir_por_zero_errado():
+    assert dividir(10, 0) == 0 
