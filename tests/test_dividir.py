439473f6de8db1a55d810265a9046df4730245f4
Rainha5 <rainha5@LAPTOP-IAV9S8SC.localdomain>
2026-03-24 21:57:09 +0000

    teste

--- tests/test_dividir.py
+++ tests/test_dividir.py
@@ -3,16 +3,3 @@ from functions.dividir import dividir
 
 def test_dividir_normal():
     assert dividir(10, 2) == 5
-
-def test_dividir_por_zero():
-    with pytest.raises(ValueError):
-        dividir(10, 0)
-
-def test_dividir_resultado_errado():
-    assert dividir(10, 2) == 6  
-
-def test_dividir_negativos():
-    assert dividir(-10, 2) == 10  
-
-def test_dividir_por_zero_errado():
-    assert dividir(10, 0) == 0 
