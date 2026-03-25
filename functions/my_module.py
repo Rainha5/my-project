94fae21f697fa45499c5e1cf3e387c6fed05c545
Rainha5 <rainha5@LAPTOP-IAV9S8SC.localdomain>
2026-03-25 01:26:42 +0000

    Exercício: testar autofix

--- functions/my_module.py
+++ functions/my_module.py
@@ -1,2 +1,7 @@
-def add(a, b):
-    return a + b
+def calcular_media(lista):
+    total = sum(lista)
+    return total / len(lista)  
+
+
+def obter_elemento(lista, indice):
+    return lista[indice] 
--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,4 +1,19 @@
-from functions.my_module import add
+import pytest
+from functions.my_module import calcular_media, obter_elemento
 
-def test_add():
-    assert add(2, 3) == 5
+
+def test_media_normal():
+    assert calcular_media([2, 4, 6]) == 4
+
+
+def test_media_lista_vazia():
+    assert calcular_media([]) == 0
+
+
+def test_obter_elemento_valido():
+    assert obter_elemento([10, 20, 30], 1) == 20
+
+
+def test_obter_elemento_invalido():
+    with pytest.raises(IndexError):
+        obter_elemento([1, 2, 3], 5)
